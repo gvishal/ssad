cbacf6dc41224437a7b886a2ddd5ea9d2da1da7b
Vishal Gupta <vishal.vgu+github@gmail.com>
2014-08-18 22:05:27 +0530

    Initial commit

---
+++ pacman_game.py
@@ -0,0 +1,30 @@
+from pacman_board import board,printBoard,generateWall,generateCoin
+
+generateWall(board)
+
+generateCoin(board,20)
+
+printBoard(board)
+
+
+class Person():
+    pass
+
+
+class Pacman(Person):
+    pass
+
+class Ghost(Person):
+    pass
+
+def checkGhost():
+    pass
+
+def collectCoin():
+    pass
+
+def ghostPosition():
+    pass
+
+def checkWall():
+    pass
