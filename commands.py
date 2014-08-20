f2a2e10963aac36ac4bbdc5f821fbdeb9716d354
Vishal Gupta <vishal.vgu+github@gmail.com>
2014-08-21 03:24:42 +0530

    Squashed ls bugs

--- commands.py
+++ commands.py
@@ -11,72 +11,88 @@ import string
 
 command = raw_input()
 command = command.split(' ')
+#print command
 
 def check_path(path):
     return os.path.exists(path)
 
 if command[0] == 'ls':
-    if command[1] == '-l':
-        """owner group others  owner group size month day time filename"""
-        if command[2]:
+    try:
+        if command[1] == '-l':
+            """ls -l (path)"""
+            """owner group others hardlinks owner group size month day time filename"""
             try:
-                check_path(command[2])
-            except:
-            dir_list = os.listdir(command[2])
-
+                path = command[2]
+                if os.path.exists(path):   
+                    dir_list = os.listdir(path)
+                    for i in xrange(len(dir_list)):
+                        dir_list[i] = path + '/' + dir_list[i]
+                else:
+                    raise IOError("specified directory doesnt exist")
+            except IndexError:
+                dir_list = os.listdir(os.getcwd())
+                for i in xrange(len(dir_list)):
+                    dir_list[i] = os.getcwd() + '/' + dir_list[i]
+            for file in dir_list:
+                st = os.stat(file)
+                details = ''
+                if os.path.isdir(file):
+                    details += 'd'
+                else:
+                    details += '-'
+                if bool(st.st_mode & stat.S_IRUSR):
+                    details += 'r'
+                else:
+                    details += '-'
+                if bool(st.st_mode & stat.S_IWUSR):
+                    details += 'w'
+                else:
+                    details += '-'
+                if bool(st.st_mode & stat.S_IXUSR):
+                    details += 'x'
+                else:
+                    details += '-'
+                if bool(st.st_mode & stat.S_IRGRP):
+                    details += 'r'
+                else:
+                    details += '-'
+                if bool(st.st_mode & stat.S_IWGRP):
+                    details += 'w'
+                else:
+                    details += '-'
+                if bool(st.st_mode & stat.S_IXGRP):
+                    details += 'x'
+                else:
+                    details += '-'
+                if bool(st.st_mode & stat.S_IROTH):
+                    details += 'r'
+                else:
+                    details += '-'
+                if bool(st.st_mode & stat.S_IWOTH):
+                    details += 'w'
+                else:
+                    details += '-'
+                if bool(st.st_mode & stat.S_IXOTH):
+                    details += 'x'
+                else:
+                    details += '-'
+                details += ' ' + str(st.st_nlink).rjust(2) + ' ' + pwd.getpwuid(st.st_uid).pw_name.ljust(4) + ' ' + grp.getgrgid(st.st_gid).gr_name.ljust(4)
+                details += ' ' + str(st.st_size).rjust(6) + ' ' + time.strftime("%B %d %H:%M", time.gmtime(st.st_mtime)).ljust(18)
+                details += ' ' + file.split('/')[-1]
+                print details
         else:
+            raise
+    except:
+        """ls (path)"""
+        try:
+            path = command[1]
+            if os.path.exists(path):
+                dir_list = os.listdir(path)
+            else:
+                raise IOError("specified directory doesnt exist")
+        except IndexError:
             dir_list = os.listdir(os.getcwd())
-        for file in dir_list:
-            filepath = os.getcwd() + '/' + file
-            st = os.stat(filepath)
-            details = ''
-            if os.path.isdir(filepath):
-                details += 'd'
-            else:
-                details += '-'
-            if bool(st.st_mode & stat.S_IRUSR):
-                details += 'r'
-            else:
-                details += '-'
-            if bool(st.st_mode & stat.S_IWUSR):
-                details += 'w'
-            else:
-                details += '-'
-            if bool(st.st_mode & stat.S_IXUSR):
-                details += 'x'
-            else:
-                details += '-'
-            if bool(st.st_mode & stat.S_IRGRP):
-                details += 'r'
-            else:
-                details += '-'
-            if bool(st.st_mode & stat.S_IWGRP):
-                details += 'w'
-            else:
-                details += '-'
-            if bool(st.st_mode & stat.S_IXGRP):
-                details += 'x'
-            else:
-                details += '-'
-            if bool(st.st_mode & stat.S_IROTH):
-                details += 'r'
-            else:
-                details += '-'
-            if bool(st.st_mode & stat.S_IWOTH):
-                details += 'w'
-            else:
-                details += '-'
-            if bool(st.st_mode & stat.S_IXOTH):
-                details += 'x'
-            else:
-                details += '-'
-            details += ' ' + str(st.st_nlink) + ' ' + pwd.getpwuid(st.st_uid).pw_name + ' ' + grp.getgrgid(st.st_gid).gr_name
-            details += ' ' + str(st.st_size).rjust(6) + ' ' + time.strftime("%B %d %H:%M", time.gmtime(st.st_mtime)) + ' ' + file
-            print details
-    else:
-        dir_list = os.listdir(os.getcwd())
-        # dir_list = os.listdir(command[1])
-        col_width = max(len(file) for file in dir_list) + 2 
+        #col_width = max(len(file) for file in dir_list) + 2 
         for file in dir_list:
             print file
             # print string.ljust(file,col_width),
