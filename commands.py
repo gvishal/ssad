6439d0096885a69cf0710580c9d759a0e6ff1cb4
Vishal Gupta <vishal.vgu+github@gmail.com>
2014-08-22 22:28:36 +0530

    commands.py ready,tested almost exhautively

--- commands.py
+++ commands.py
@@ -8,12 +8,6 @@ import shutil
 import time
 import string
 
-"""ls command"""
-
-command = raw_input().strip()
-command = command.split(' ')
-# print command
-
 def check_path(command, pos):
     """Returns path if it exists or error or False if it is not specified"""
     try:
@@ -50,167 +44,217 @@ def check_option(command, pos):
         option = False
     return option
 
-def move(src, dst):
-    """This code has been taken from shutil.py and modified for my use.
-    Recursively move a file or directory to another location. This is
-    similar to the Unix "mv" command.
-
-    If the destination is a directory or a symlink to a directory, the source
-    is moved inside the directory. The destination path "must not" already
-    exist.
-
-    If the destination already exists but is not a directory, it may be
-    overwritten depending on os.rename() semantics.
-
-    If the destination is on our current filesystem, then rename() is used.
-    Otherwise, src is copied to the destination and then removed.
-    A lot more could be done here...  A look at a mv.c shows a lot of
-    the issues this implementation glosses over.
-
-    """
-    real_dst = dst
-    if os.path.isdir(dst):
-        if _samefile(src, dst):
-            # We might be on a case insensitive filesystem,
-            # perform the rename anyway.
-            os.rename(src, dst)
-            return
-
-        real_dst = os.path.join(dst, _basename(src))
-        if os.path.exists(real_dst):
-            raise Error, "Destination path '%s' already exists" % real_dst
+def list_files(command, startpath):
     try:
-        os.rename(src, real_dst)
-    except OSError:
-        if os.path.isdir(src):
-            if _destinsrc(src, dst):
-                raise Error, "Cannot move a directory '%s' into itself '%s'." % (src, dst)
-            copytree(src, real_dst, symlinks=True)
-            rmtree(src)
-        else:
-            copy2(src, real_dst)
-            os.unlink(src)
+        if not os.path.isdir(startpath):
+            raise
+        pre = '#--------------------'
+        post = '-------------------#'
+        i = 0 
+        for root, dirs, files in os.walk(startpath):
+            level = root.replace(startpath, '').count(os.sep)
+            indent = ' ' * 2 *(level)
+            i += 1
+            if indent:
+                print '{}{}'.format(indent, '|')
+            print '{}{} Folder name: {}/{}'.format(indent, pre, os.path.basename(root), post)
+            subindent = ' ' * 2 *(level + 1)
+            if not dirs and not files:
+                print '\n{}{}'.format(subindent*5, '(EMPTY FOLDER)')
+            for f in files:
+                print '{}{}'.format(subindent, '|')
+                try:
+                    path = os.path.join(root, f)
+                    if os.path.exists(path):
+                        complete_path = path
+                except:
+                    complete_path = os.path.join(os.getcwd(), path)
+                st = os.stat(complete_path)
+                details = f + str(st.st_size).rjust(6) + ' bytes ' + time.strftime("%B %d %H:%M", time.gmtime(st.st_mtime)).ljust(16)
+                #print '{}#-{}'.format(subindent, f)
+                print '{}#-{}'.format(subindent, details)
+    except:
+        print "%s: directory does not exist" % command[0]
+        sys.exit(0)
 
-if command[0] == 'ls':
+def file_details(command, path, root=None):
+    """Returns file details in ls -l format"""
     try:
-        if command[1] == '-l':
-            """ls -l (path)"""
-            """owner group others hardlinks owner group size month day time filename"""
+        try:
+            if os.path.exists(path):
+                complete_path = path
+        except:
             try:
-                path = command[2]
-                if os.path.exists(path):   
-                    dir_list = os.listdir(path)
-                    for i in xrange(len(dir_list)):
-                        dir_list[i] = path + '/' + dir_list[i]
+                path = os.path.join(root, path)
+                if os.path.exists(path):
+                    complete_path = path
                 else:
-                    raise IOError("specified directory doesnt exist")
+                    complete_path = os.path.join(os.getcwd(), path)
+            except:
+                print "Invalid path specified"
+                sys.exit(0)
+        st = os.stat(complete_path)
+        details = ''
+        if os.path.isdir(complete_path):
+            details += 'd'
+        else:
+            details += '-'
+        if bool(st.st_mode & stat.S_IRUSR):
+            details += 'r'
+        else:
+            details += '-'
+        if bool(st.st_mode & stat.S_IWUSR):
+            details += 'w'
+        else:
+            details += '-'
+        if bool(st.st_mode & stat.S_IXUSR):
+            details += 'x'
+        else:
+            details += '-'
+        if bool(st.st_mode & stat.S_IRGRP):
+            details += 'r'
+        else:
+            details += '-'
+        if bool(st.st_mode & stat.S_IWGRP):
+            details += 'w'
+        else:
+            details += '-'
+        if bool(st.st_mode & stat.S_IXGRP):
+            details += 'x'
+        else:
+            details += '-'
+        if bool(st.st_mode & stat.S_IROTH):
+            details += 'r'
+        else:
+            details += '-'
+        if bool(st.st_mode & stat.S_IWOTH):
+            details += 'w'
+        else:
+            details += '-'
+        if bool(st.st_mode & stat.S_IXOTH):
+            details += 'x'
+        else:
+            details += '-'
+        #improve the following spaghetti code
+        details += ' ' + str(st.st_nlink).rjust(2) + ' ' + pwd.getpwuid(st.st_uid).pw_name.ljust(4) + ' ' + grp.getgrgid(st.st_gid).gr_name.ljust(4)
+        details += ' ' + str(st.st_size).rjust(6) + ' ' + time.strftime("%B %d %H:%M", time.gmtime(st.st_mtime)).ljust(18)
+        details += ' ' + path.split('/')[-1]
+        return details
+    except:
+        print "Path does not exist"
+
+def main():
+    try:
+        command = raw_input().strip()
+        command = command.split()
+    except:
+        print "Enter correct command"
+        sys.exit(0)
+    if command[0] == 'ls':
+        """ls command"""
+        try:
+            if command[1] == '-l':
+                """ls -l (path)"""
+                """owner group others hardlinks owner group size month day time filename"""
+                try:
+                    path = command[2]
+                    try:
+                        if os.path.exists(path):
+                            dir_list = os.listdir(path)
+                            for i in xrange(len(dir_list)):
+                                dir_list[i] = os.path.join(path, dir_list[i])
+                    except:
+                        print "%s: specified directory doesnt exist" % command[0]
+                        sys.exit(0)
+                except IndexError:
+                    dir_list = os.listdir(os.getcwd())
+                    for i in xrange(len(dir_list)):
+                        dir_list[i] = os.path.join(os.getcwd(), dir_list[i])
+                for file in dir_list:
+                    details = file_details(command, file)
+                    print details
+            else:
+                raise
+        except:
+            """ls (path)"""
+            try:
+                path = command[1]
+                if os.path.exists(path):
+                    dir_list = os.listdir(path)
+                else:
+                    print "%s: specified directory doesnt exist" % command[0]
+                    sys.exit(0)
             except IndexError:
                 dir_list = os.listdir(os.getcwd())
-                for i in xrange(len(dir_list)):
-                    dir_list[i] = os.getcwd() + '/' + dir_list[i]
+            #col_width = max(len(file) for file in dir_list) + 2 
             for file in dir_list:
-                st = os.stat(file)
-                details = ''
-                if os.path.isdir(file):
-                    details += 'd'
-                else:
-                    details += '-'
-                if bool(st.st_mode & stat.S_IRUSR):
-                    details += 'r'
-                else:
-                    details += '-'
-                if bool(st.st_mode & stat.S_IWUSR):
-                    details += 'w'
-                else:
-                    details += '-'
-                if bool(st.st_mode & stat.S_IXUSR):
-                    details += 'x'
-                else:
-                    details += '-'
-                if bool(st.st_mode & stat.S_IRGRP):
-                    details += 'r'
-                else:
-                    details += '-'
-                if bool(st.st_mode & stat.S_IWGRP):
-                    details += 'w'
-                else:
-                    details += '-'
-                if bool(st.st_mode & stat.S_IXGRP):
-                    details += 'x'
-                else:
-                    details += '-'
-                if bool(st.st_mode & stat.S_IROTH):
-                    details += 'r'
-                else:
-                    details += '-'
-                if bool(st.st_mode & stat.S_IWOTH):
-                    details += 'w'
-                else:
-                    details += '-'
-                if bool(st.st_mode & stat.S_IXOTH):
-                    details += 'x'
-                else:
-                    details += '-'
-                details += ' ' + str(st.st_nlink).rjust(2) + ' ' + pwd.getpwuid(st.st_uid).pw_name.ljust(4) + ' ' + grp.getgrgid(st.st_gid).gr_name.ljust(4)
-                details += ' ' + str(st.st_size).rjust(6) + ' ' + time.strftime("%B %d %H:%M", time.gmtime(st.st_mtime)).ljust(18)
-                details += ' ' + file.split('/')[-1]
-                print details
+                print file
+                # print string.ljust(file,col_width),
+
+    elif command[0] == 'cp':
+        """cp command"""
+        option = check_option(command, 1)
+        if option == '-r':
+            src = check_src(command, 2)
+            dest = check_dest(command, 3)
+            shutil.copytree(src, dest)
+        elif not option:
+            print "%s: missing file operand" % command[0]
+            sys.exit(0)
         else:
-            raise
-    except:
-        """ls (path)"""
-        try:
-            path = command[1]
-            if os.path.exists(path):
-                dir_list = os.listdir(path)
-            else:
-                raise IOError("specified directory doesnt exist")
-        except IndexError:
-            dir_list = os.listdir(os.getcwd())
-        #col_width = max(len(file) for file in dir_list) + 2 
-        for file in dir_list:
-            print file
-            # print string.ljust(file,col_width),
+            src = check_src(command, 1)
+            if os.path.isdir(src):
+                print "cp: omitting directory %s" % src
+                sys.exit(0)
+            dest = check_dest(command, 2)
+            shutil.copy(src, dest)
 
-elif command[0] == 'cp':
-    option = check_option(command, 1)
-    if option == '-r':
-        src = check_src(command, 2)
-        dest = check_dest(command, 3)
-        shutil.copytree(src, dest)
-    elif not option:
-        print "%s: missing file operand" % command[0]
-        sys.exit(0)
-    else:
+    elif command[0] == 'mv':
+        """move and rename"""
         src = check_src(command, 1)
-        if os.path.isdir(src):
-            print "cp: omitting directory %s" % src
-            sys.exit(0)
         dest = check_dest(command, 2)
-        shutil.copy(src, dest)
+        try:
+            shutil.move(src, dest)
+        except shutil.Error as e:
+            print "mv: %s" % e
 
-elif command[0] == 'mv':
-    """move and rename"""
-    src = check_src(command, 1)
-    dest = check_dest(command, 2)
-    try:
-        shutil.move(src, dest)
-    except shutil.Error as e:
-        print "mv: %s" % e
+    elif command[0] == 'rm':
+        """rm command"""
+        option = check_option(command, 1)
+        if option == '-r':
+            src = check_src(command, 2)
+            shutil.rmtree(src)
+        elif not option:
+            print "%s: missing file operand" % command[0]
+        else:
+            src = check_src(command, 1)
+            try:
+                os.remove(src)
+            except OSError:
+                print "%s: cannot remove '%s': Is a directory" % (command[0],command[1])
+                sys.exit(0)
 
-elif command[0] == 'rm':
-    option = check_option(command, 1)
-    if option == '-r':
-        src = check_src(command, 2)
-        shutil.rmtree(src)
-    elif not option:
-        print "%s: missing file operand" % command[0]
-    else:
-        src = check_src(command, 1)
-        if os.path.isdir(src):
-            print "%s: cannot remove %s: Is a directory" % (command[0],command[1])
-            sys.exit(0)
-        os.remove(src)
+    elif command[0] == 'dirstr':
+        """dirstr command"""
+        try:
+            startpath = command[1]
+        except:
+            startpath = os.getcwd()
+        list_files(command, startpath)
 
-elif command[0] == 'dirstr':
-    pass
+    #Bonus commands
+    elif command[0] == 'pwd':
+        print os.getcwd()
+
+    elif command[0] == 'mkdir':
+        try:
+            d = command[1]
+            if not os.path.exists(d):
+                os.makedirs(d)
+            else:
+                print "%s: cannot create directory '%s': File exists" % (command[0],d)
+        except IndexError:
+            print "%s: missing operand" % command[0]
+
+if __name__ == '__main__':
+    main()
---
+++ readme.txt
@@ -0,0 +1,31 @@
+-Part A My Pacman
+
+*********Reload board keeping score intact
+
+-Inheritance done via classes
+-Polymorphism
+-Modularity
+-Encapsulation
+-Functionalities of the game
+
+*Bonus*
+-Mulitple ghosts
+
+
+
+
+
+
+
+
+
+
+-Part B My Commands
+
+-Implemented ls, ls -l, mv, cp(and cp -r) and rm(and rm -r).
+-Implemented dirstr
+-Errors handled correctly
+
+*Bonus*
+-dirstr displays file size and last modified date
+-Implemented pwd and mkdir
